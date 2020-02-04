9b9a46b996ef355c0677fa720c3e9a2c53880e58
JelleStiesri <jellestiesri@gmail.com>
2020-02-04 15:04:10 +0100

    opdracht 3 af

--- Form1.py
+++ Form1.py
@@ -1,4 +1,15 @@
-#1
+"Ik heb sommige dingen samen gedaan met Max."
+
+#1 Pyramide
+def pyramide():
+    hoogte = int(input("Hoe hoog is de Pyramide: "))
+    for item in range(hoogte):
+        print('*'*(item+1))
+    lengte = hoogte-1
+    for item in range(hoogte):
+        print('*'*(item-1))
+
+pyramide()
 #?
 
 #2 Testcheck WERKT
@@ -17,18 +28,41 @@ def testcheck():
     index = index + 1
     print(index)
 
-#2 Lijstcheck
+#3 Lijstcheck
 
-#a
-def count():
-    lijst = [4,3,6,3,5,7,5,4,3,2,7,2,9,5,3,2,1,2,4,5,7,3,4,6,3]
-    nummer = int(input('Welk nummer wil je tellen?: '))
-    aantal = lijst.count(nummer)
-    print('getal: {} komt {} keer voor in de lijst'.format(nummer,aantal))
-#b
+#a WERKT
+def count(lst,nummer):
+    aantal = lst.count(nummer)
+    return aantal
+    #print('getal: {} komt {} keer voor in de lijst'.format(nummer,aantal))
+"""De uitvoering van de functie count staat hier onder, om te testen verwijder '#',
+Dit heb ik gedaan omdat de functie ook word gebruikt voor opdracht C can 3,
+ daardoor is de print ook als opmerking geplaatst"""
+#count([4,3,6,3,5,7,5,4,3,2,7,2,9,5,3,2,1,2,4,5,7,3,4,6,3],3)
+
+#b WERKT
 def verschil():
     lijst = [4, 3, 6, 3, 5, 7, 5, 4, 3, 2, 7, 2, 9, 5, 3, 2, 1, 2, 4, 5, 7, 3, 4, 6, 3]
     index = 1
-    for item in list 
+    hoogste = 0
+    for item in lijst[:len(lijst)-1]:
+        verschil = abs(item - lijst[index])
+        if verschil > hoogste:
+            hoogste = verschil
+            print(hoogste)
+        index = index + 1
+    print('Het grootste verschil tussen 2 opeenvolgende getallen = {}'.format(hoogste))
 
-verschil()
+#c WERKT
+def eennulcheck():
+    lst = [1,1,0,1,0,1,0,0,0,1,1,1,0,1,0,1,0,0,0,1,1,0,1,1,0]
+    aantal1 = count(lst,1)
+    aantal0 = count(lst,0)
+    if aantal1 > aantal0 and aantal0 <= 12:
+        return True
+    else:
+        return False
+#print(eennulcheck())
+
+#4 Palindroom
+
